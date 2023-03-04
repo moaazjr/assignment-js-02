32b9e86866049c9a0fa5e67c193c545e15e44ce6
moaaz.jr <124199877+moaazjr@users.noreply.github.com>
2023-03-04 22:51:13 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.body.addEventListener('keydown', function(e){
+    if(e.code == 'Space'){
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        document.body.style.backgroundColor = "#" + randomColor;
+
+    }
+} )
